f8b4a4e4068ff7925528919bdcf090ac3b24f965
Advaith-Hello <advaith.anoopr@gmail.com>
2026-01-12 22:00:39 -0600

    Added needed augments to augments.py

--- src/my_project/augments.py
+++ src/my_project/augments.py
@@ -9,12 +9,18 @@ from jax import jit
 
 @partial(jit, static_argnums=1)
 def pad_to(x, resize_shape, key):
-    resize_pad_1 = resize_shape - x.shape // 2
-    resize_pad_2 = resize_shape - x.shape - resize_pad_1
-    return jnp.pad(x, mode="empty", pad_width=(
+    resize_pad_x = jnp.maximum(resize_shape[0] - x.shape[1], 0)
+    resize_pad_y = jnp.maximum(resize_shape[1] - x.shape[2], 0)
+
+    resize_pad_x_1 = resize_pad_x // 2
+    resize_pad_x_2 = resize_pad_x - resize_pad_x_1
+    resize_pad_y_1 = resize_pad_y // 2
+    resize_pad_y_2 = resize_pad_y - resize_pad_y_1
+
+    return jnp.pad(x, mode="constant", pad_width=(
         (0, 0),
-        (resize_pad_1, resize_pad_2),
-        (resize_pad_1, resize_pad_2),
+        (resize_pad_x_1, resize_pad_x_2),
+        (resize_pad_y_1, resize_pad_y_2),
         (0, 0),
     )), key
 
@@ -23,11 +29,59 @@ def pad_to(x, resize_shape, key):
 def crop_to(x, crop_shape, key):
     key, k1, k2 = jrd.split(key, num=3)
     start_indices = (
-        jrd.randint(k1, shape=(), minval=0, maxval=x.shape[0] - crop_shape[0] + 1),
-        jrd.randint(k2, shape=(), minval=0, maxval=x.shape[1] - crop_shape[1] + 1),
+        0,
+        jrd.randint(k1, shape=(), minval=0, maxval=x.shape[1] - crop_shape[0] + 1),
+        jrd.randint(k2, shape=(), minval=0, maxval=x.shape[2] - crop_shape[1] + 1),
+        0,
     )
     return lax.dynamic_slice(
         x,
         start_indices=start_indices,
         slice_sizes=crop_shape,
+    ), key
+
+
+@partial(jit, static_argnums=1)
+def vertical_flip(x, p, key):
+    key, subkey = jrd.split(key, num=2)
+    return (
+        jnp.flip(x, axis=1)
+        if jrd.uniform(subkey) < p
+        else x
+    ), key
+
+
+@partial(jit, static_argnums=1)
+def horizontal_flip(x, p, key):
+    key, subkey = jrd.split(key, num=2)
+    return (
+        jnp.flip(x, axis=2)
+        if jrd.uniform(subkey) < p
+        else x
+    ), key
+
+
+@partial(jit, static_argnums=1)
+def cutout(x, cutout_proportion, key):
+    key, k1, k2 = jrd.split(key, num=3)
+
+    cut_h, cut_w = (
+        jnp.int32(cutout_proportion * x.shape[1]),
+        jnp.int32(cutout_proportion * x.shape[2]),
+    )
+
+    y_start = jrd.randint(k1, (), minval=0, maxval=(x.shape[1] - cut_h + 1))
+    x_start = jrd.randint(k2, (), minval=0, maxval=(x.shape[2] - cut_w + 1))
+
+    cutout_patch = jnp.zeros(shape=(
+        x.shape[0],
+        cut_h,
+        cut_w,
+        x.shape[3]
+    ), dtype=x.dtype)
+
+    return lax.dynamic_update_slice(
+        x,
+        update=cutout_patch,
+        start_indices=(0, y_start, x_start, 0),
     ), key
--- src/my_project/samples/sample_augments.py
+++ src/my_project/samples/sample_augments.py
@@ -7,9 +7,6 @@ augment1 = (
     ("pad_to", (32, 32)),
     ("crop_to", (28, 28)),
     ("horizontal_flip", 0.5),
-    ("translate", (1, 3)),
-    ("rotate", (-12, 12)),
-    ("scale", (0.9, 1.1)),
-    ("cutout", (0.1, 0.2)),
+    ("cutout", (0.15, 0.15)),
     ("gaussian_noise", (0.01, 0.05)),
 )
